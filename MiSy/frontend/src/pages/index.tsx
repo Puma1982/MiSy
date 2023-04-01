8f9f04381967033ccd445441ee9415e871cf5adf
VIP-HACKER <griss_hai2005@hotmail.com>
2023-04-01 20:54:34 +0200

    Initial Commit
    59:43
    <button>Login
    <button>Logout

---
+++ MiSy/frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+const Home : NextPage = () => {   
+  const { data } = useSession();
+
+  console.log("HERE IS DATA", data);
+return ( 
+  <div>{data?.user ?(
+    <button onClick={() => signOut()}>Sign Out</button>
+    ) : (
+   <button onClick={() => signIn("google")}>Sign In</button>
+    )}
+   {data?.user?.name}
+   </div>
+  );
+};
+export default Home;
