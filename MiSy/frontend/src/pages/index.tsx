64f446ed69ce00c592963b1d4dda61a9c7e93209
VIP-HACKER <griss_hai2005@hotmail.com>
2023-04-02 09:50:14 +0200

    Nothing changed
    1:27:53

--- MiSy/frontend/src/pages/index.tsx
+++ MiSy/frontend/src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage, NextPageContext } from 'next';
 import { getSession, useSession } from 'next-auth/react';
 import Auth from '../components/Auth/Auth';
 import Chat from '../components/Chat/Chat';
+import { Session } from 'next-auth';
 
 
 const Home: NextPage = () => {
@@ -10,14 +11,12 @@ const Home: NextPage = () => {
 
   console.log('HERE IS DATA', data);
 
-  return <Box>
-    {data?.user ? <Chat /> : <Auth />}
-  </Box>;
+  return <Box>{data?.user ? <Chat /> : <Auth />}</Box>;
   };
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
-  
+
   return{
     props: {
       session,
