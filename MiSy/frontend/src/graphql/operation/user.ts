135758495d6e617aa19f71ff9c2c73cff674eed9
VIP-HACKER <griss_hai2005@hotmail.com>
2023-04-03 19:22:36 +0200

    Frontand+

---
+++ MiSy/frontend/src/graphql/operation/user.ts
@@ -0,0 +1,25 @@
+import { gql } from "@apollo/client";
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  Queries: {
+    searchUsers: gql`
+      query SearchUsers($username: String!) {
+        searchUsers(username: $username) {
+          id
+          username
+        }
+      }
+    `,
+  },
+  Mutations: {
+    createUsername: gql`
+      mutation CreateUsername($username: String!) {
+        createUsername(username: $username) {
+          success
+          error
+        }
+      }
+    `,
+  },
+  Subscriptions: {},
+};
